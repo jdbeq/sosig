ba21b91ac5ceadd3714522bdb44d3ab06a2abf80
JD Beckett <jimbzy.mail@gmail.com>
2020-11-08 20:10:28 -0600

    Hello, World!

--- kernel/kernel_main/kernel_main.c
+++ kernel/kernel_main/kernel_main.c
@@ -1,4 +1,15 @@
+#include "uart.h"
+#include "mbox.h"
+
 void kernel_main()
 {
+    /* Initialize serial console.*/
+    uart_init();
+    uart_puts("Hello, world!");
 
+    while(1) 
+    {
+        uart_send(uart_getc());
+    }
+    
 }
