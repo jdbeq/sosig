48c3fae748ad46c11f724ab77cd142e1d27a7eae
JD Beckett <jimbzy.mail@gmail.com>
2020-12-27 23:55:17 -0600

    Added stdlib abs()

---
+++ kernel/include/stdlib.h
@@ -0,0 +1,11 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+ 
+#ifndef STDLIB_H
+#define STDLIB_H
+
+int abs(int i);
+
+#endif
---
+++ kernel/libc/stdlib/stdlib.c
@@ -0,0 +1,14 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+
+int abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * (-1);
+		return i;
+	}
+	else { return i;}
+}
